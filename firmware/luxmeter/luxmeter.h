b1f49c79e5d22d7f750f1de6fe9f54b9aa4d330d
Sean Caulfield <sean@yak.net>
2015-04-27 19:09:10 -0400

    initial skeleton commit

---
+++ firmware/luxmeter/luxmeter.h
@@ -0,0 +1,7 @@
+#ifndef __LUXMETER_H__
+#define __LUXMETER_H__
+
+#include <Arduino.h>
+#include <Wire.h>
+
+#endif
---
+++ firmware/luxmeter/luxmeter.ino
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+#include "luxmeter.h"
+
+void setup() {
+}
+
+void loop() {
+}
